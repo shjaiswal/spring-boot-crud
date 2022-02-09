461cf776adb19ce222c8e282d36fd02643210451
Shubhangi Jaiswal <shubhangi751@gmail.com>
2022-02-08 20:20:41 -0500

    Task: Setup of the project

---
+++ CrudApp/src/test/java/com/mycompany/CrudApp/CrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.CrudApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
